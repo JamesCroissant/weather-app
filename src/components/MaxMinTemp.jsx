6cf0dc2d25d8dac68fd6eff7ae16347818d34314
JamesCroissant <yuham1014@gmail.com>
2024-03-05 17:17:50 -0800

    add max and min temperature component

---
+++ src/components/MaxMinTemp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MaxMinTemp = ({ currentWeather, isCelsius }) => {
+  return (
+    <div className="flex items-center justify-around space-x-7">
+      <div className="flex flex-col py-5">
+        <div className="text-lg">HIGH -</div>
+        <div className="font-semibold text-2xl">{isCelsius ? `${currentWeather.maxCelsius}°C` : `${currentWeather.maxFahrenheit}°F`}</div>
+      </div>
+      <div className="flex flex-col py-5">
+        <div className="text-lg">LOW -</div>
+        <div className="font-semibold text-2xl">{isCelsius ? `${currentWeather.minCelsius}°C` : `${currentWeather.minFahrenheit}°F`}</div>
+      </div>
+    </div>
+  )
+}
+
+export default MaxMinTemp
