c2cdb0b09aa7cdad580ed412e0910bf1705ab0df
JamesCroissant <yuham1014@gmail.com>
2024-03-06 02:43:11 -0800

    add loading spinner component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { SyncLoader } from "react-spinners";
 
 const Loading = () => {
   return (
     <div className="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 ">
-      <div className="border-t-transparent border-solid animate-spin rounded-full border-slate-500 border-8 h-64 w-64"></div>
+      <SyncLoader color="#a3cdef" size={28} margin={10} />
     </div>
+    
   )
 }
 
